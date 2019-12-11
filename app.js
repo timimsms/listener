8e1dda4c5bf9d360fa74b7cf5e666e0462a61343
Tim Walsh <tim@mims.ms>
2019-12-10 18:59:10 -0700

    Loosen CORS settings to allow cross-site requests

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var port = process.env.PORT || 8080
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.set('port', port);
 
 app.get('/', function (req, res) {
